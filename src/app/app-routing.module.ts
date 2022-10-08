2ed67491d5544c75f393af72d47ff47c349ce81e
DDoS-73 <77987765+DDoS-73@users.noreply.github.com>
2022-10-08 14:06:06 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
 import { RegistrationComponent } from './features/auth/registration/registration.component';
 import { TodosComponent } from "./features/todos/todos/todos.component";
 
 const routes: Routes = [
   { path: 'register', component: RegistrationComponent },
-  { path: 'todos', component: TodosComponent },
-  // { path: '**', component: PageNotFoundComponent },
+  { path: 'todos', component: TodosComponent }
 ];
 
 @NgModule({
